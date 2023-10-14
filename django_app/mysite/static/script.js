6da66d6e3039c952178145bce6a70ca44b70589c
hadh93 <hoondog1993@gmail.com>
2023-10-14 19:55:30 +0900

    Web-app UI WIP

--- django_app/mysite/static/script.js
+++ django_app/mysite/static/script.js
@@ -1,10 +1,12 @@
+
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 let userMessage = null; // Variable to store user's message
-const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
+const ORG_ID = "org-AOXLawEbqxLrd5j4VqJMpH2H";
 const inputInitHeight = chatInput.scrollHeight;
 const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
@@ -19,21 +21,24 @@ const generateResponse = (chatElement) => {
     const API_URL = "https://api.openai.com/v1/chat/completions";
     const messageElement = chatElement.querySelector("p");
     // Define the properties and message for the API request
+    
     const requestOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${API_KEY}`
-        },
+            },
         body: JSON.stringify({
-            model: "gpt-3.5-turbo",
+            model: "gpt-3.5-turbo-16k",
             messages: [{role: "user", content: userMessage}],
         })
-    }
+    };
+    console.log(requestOptions);
     // Send POST request to API, get response and set the reponse as paragraph text
     fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
         messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
+    }).catch(err => {
+        console.log(err);
         messageElement.classList.add("error");
         messageElement.textContent = "Oops! Something went wrong. Please try again.";
     }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
